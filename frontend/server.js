ab718124dcafa2cff1be647fef3949275aaed39c
ValDev99 <99172923+ValDev99@users.noreply.github.com>
2025-03-25 11:21:42 +0100

    feat: update front and try to verify that pipeline is ok

--- frontend/server.js
+++ frontend/server.js
@@ -1,8 +1,27 @@
 import http from "http";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Frontend service running!");
+  if (req.url === "/") {
+    const filePath = path.join(__dirname, "index.html");
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Not Found");
+  }
 });
 
 server.listen(8080, () => {
